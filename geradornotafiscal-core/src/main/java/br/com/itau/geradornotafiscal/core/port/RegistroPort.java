dfdf50a5849842b725f85d14c2336041be67ec49
pedro-inacio11 <pedro.inacio11@gmail.com>
2024-02-03 14:59:16 -0300

    Create Registro

---
+++ geradornotafiscal-core/src/main/java/br/com/itau/geradornotafiscal/core/port/RegistroPort.java
@@ -0,0 +1,9 @@
+package br.com.itau.geradornotafiscal.core.port;
+
+import br.com.itau.geradornotafiscal.core.model.NotaFiscal;
+
+public interface RegistroPort {
+
+    void RegistrarNotaFiscal(NotaFiscal notaFiscal);
+
+}
---
+++ geradornotafiscal-out/src/main/java/br/com/itau/geradornotafiscal/out/RegistroAdapter.java
@@ -0,0 +1,19 @@
+package br.com.itau.geradornotafiscal.out;
+
+import br.com.itau.geradornotafiscal.core.model.NotaFiscal;
+import br.com.itau.geradornotafiscal.core.port.RegistroPort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistroAdapter implements RegistroPort {
+    @Override
+    public void RegistrarNotaFiscal(NotaFiscal notaFiscal) {
+
+        try {
+            //Simula o registro da nota fiscal
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
